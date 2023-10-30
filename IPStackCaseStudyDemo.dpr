b4c4f6a1908c9acaf6d5e16ea75f51e51b7f3c50
Abduraimov Muminjon <muminjonguru@outlook.com>
2023-10-30 12:31:03 +0000

    init

---
+++ IPStackCaseStudyDemo.dpr
@@ -0,0 +1,14 @@
+﻿program IPStackCaseStudyDemo;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  IPStackCaseStudyDemo.UnitMain in 'IPStackCaseStudyDemo.UnitMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
